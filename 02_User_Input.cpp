16a925e14162b56acab4b5c52bef0f4c5dff832c
Rohan2508 <mishrarm1611@gmail.com>
2023-02-05 17:38:25 +0530

    user input

---
+++ 02_User_Input.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{   
+    int number;    // Declare Variable= number
+   
+    printf("Enter an Integer value: ");  
+    
+    // reads value from user and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered the Interger: %d", number);
+    
+    return 0;
+}
