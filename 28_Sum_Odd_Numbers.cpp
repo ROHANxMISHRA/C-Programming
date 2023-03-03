f26b028f694412ed81d6425e60ab0ee0a943556b
Rohan2508 <mishrarm1611@gmail.com>
2023-03-03 15:56:07 +0530

    Sum Odd Numbers

---
+++ 28_Sum_Odd_Numbers.cpp
@@ -0,0 +1,22 @@
+/* C program to print the sum of all odd numbers from 1 to n  */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n, sum=0;
+
+    /* Input range to find sum of odd numbers */
+    printf("\n Enter upper limit: ");
+    scanf("%d", &n);
+
+    /* Find the sum of all odd number */
+    for(i=1; i<=n; i+=2)
+    {
+        sum += i;
+    }
+
+    printf("\n Sum of odd numbers = %d", sum);
+
+    return 0;
+}
