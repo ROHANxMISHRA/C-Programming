61e297bf8525112120552dc294aada2d78934ff6
Rohan2508 <mishrarm1611@gmail.com>
2023-03-19 21:12:09 +0530

    Find Length String

---
+++ 46_Find_Length_String.cpp
@@ -0,0 +1,57 @@
+/**
+ * C program to find length of a string using for loop
+ */
+
+#include<stdio.h>
+#include<string.h>
+#define MAX_SIZE 100 // Maximum size of the string
+
+int main()
+{
+    char text[MAX_SIZE]; /* Declares a string of size 100 */
+    int i;
+    int count= 0;
+
+    /* Input a string from user */
+    printf("\n Enter any string: ");
+    gets(text);
+
+    /* Iterate till the last character of string */
+    for(i=0; text[i]!='\0'; i++)
+    {
+        count++;
+    }
+
+    printf("\n Length of '%s' = %d", text, count);
+
+    return 0;
+}
+
+
+/**
+ * C program to find length of a string using while loop
+ */
+
+/*#include <stdio.h>
+#define MAX_SIZE 100 // Maximum size of the string
+
+int main()
+{
+    char text[MAX_SIZE]; /* Declares a string of size 100 
+    int index= 0;
+
+    /* Input string from user 
+    printf("Enter any string: ");
+    gets(text);   
+
+    /* Iterate though last element of the string 
+    while(text[index] != '\0')
+    {
+        index++;
+    }
+
+    printf("Length of '%s' = %d", text, index);
+
+    return 0;
+}  
+ */
