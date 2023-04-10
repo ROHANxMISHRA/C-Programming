8b8cfd233d41733c821ff1a3daac336d691375ad
Rohan2508 <mishrarm1611@gmail.com>
2023-04-10 21:52:26 +0530

    Class Object Creatiopn

---
+++ 66_Class_Object_Creation.cpp
@@ -0,0 +1,46 @@
+// Object and Class in C++ Programming
+// Program to illustrate the working of
+// objects and class in C++ Programming
+
+#include <iostream>    // Header File
+using namespace std;    //  its contains cout, cin, endl
+
+// create a class
+class Room 
+{
+   public:                // Access specifier
+    double length;        // Definig Data Members of class
+    double breadth;
+    double height;
+
+    double calculateArea()         // Defining Methods/ Data Functions
+	{
+        return length * breadth;
+    }
+
+    double calculateVolume() 
+	{
+        return length * breadth * height;
+    }
+};
+
+int main() 
+{
+
+    // create object of Room class
+    Room room1;
+
+    // assign values to data members
+    room1.length = 42.5;
+    room1.breadth = 30.8;
+    room1.height = 19.2;
+    
+    
+
+    // calculate and display the area and volume of the room
+    cout << "Area of Room =  " << room1.calculateArea()<< endl;
+    cout << "Volume of Room =  " << room1.calculateVolume() << endl;
+
+    return 0;
+}
+
