cd9be22cc638aa56ecd0a8684a2731ada3f40468
Rohan2508 <mishrarm1611@gmail.com>
2023-04-18 21:39:04 +0530

    Code

---
+++ 71_ code.cpp
@@ -0,0 +1,12 @@
+//write a c program to add two numbers?
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a = 3;
+  int b = 5;
+  int sum;
+  sum = a + b;
+  cout<<"The sum is: "<<sum<<endl;
+  return 0;
+}
